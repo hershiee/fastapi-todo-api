d99e0b645a1ab9a874f842c8fcd79fc200a0e54a
Harshita Gupta <gharshita035@gmail.com>
2026-03-27 21:30:10 +0530

    Authenticate Requests

--- TODOAPP/routers/auth.py
+++ TODOAPP/routers/auth.py
@@ -55,9 +55,9 @@ def authenticate_user(username: str, password: str, db):
         return False
     return user
 
-def create_access_token(username:str, user_id:int, expires_delta:timedelta):
+def create_access_token(username:str, user_id:int, role:str, expires_delta:timedelta):
 
-    encode = {'sub': username, 'id': user_id}
+    encode = {'sub': username, 'id': user_id, 'role': role}
     expire = datetime.now(timezone.utc) + expires_delta
     encode.update({'exp': expire})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
@@ -67,9 +67,10 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get('sub')
         user_id: int = payload.get('id')
+        user_role: str = payload.get('role')
         if username is None or user_id is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
-        return {'username': username, 'id': user_id}
+        return {'username': username, 'id': user_id, 'user_role': user_role}
     except JWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
 
@@ -95,6 +96,6 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password")
-    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
 
     return {'access_token': token, 'token_type': 'bearer'}
